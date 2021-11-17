6906cb71ff8343d9309e15255bc9705d0a4f205f
cexup <rahmanecky.r@gmail.com>
2021-11-17 17:37:27 +0700

    add view model and component listcoin ui

---
+++ app/src/main/java/com/example/simplecryptoapp/presentation/coin_list/components/CoinListItem.kt
@@ -0,0 +1,66 @@
+package com.example.simplecryptoapp.presentation.coin_list.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.simplecryptoapp.domain.model.Coin
+import com.example.simplecryptoapp.presentation.ui.theme.SimpleCryptoAppTheme
+
+@Composable
+fun CoinListItem(
+    coin : Coin,
+    onItemClick :(Coin) -> Unit,
+    m:Modifier = Modifier
+){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(coin) }
+            .padding(20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+           text = "${coin.rank}. ${coin.name} (${coin.symbol})",
+           style = MaterialTheme.typography.body1,
+           overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = if(coin.is_active) "active" else "inactive",
+            color = if(coin.is_active) Color.Green else Color.Red,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.End,
+            style = MaterialTheme.typography.body2,
+            modifier =  m.align(CenterVertically)
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun priviewListitem(){
+    SimpleCryptoAppTheme() {
+        CoinListItem(coin = Coin(
+            id = "1",
+            is_active = true,
+            name = "Bitcoin",
+            rank = 1,
+            symbol = "BTC"
+        ) , onItemClick ={} )
+    }
+
+}
